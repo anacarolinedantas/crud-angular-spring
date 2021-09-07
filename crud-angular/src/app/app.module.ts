2ceb3c8d83ea39b51214efaa00b7c0330599efea
Caroline Dantas <caroline.dantas@esig.com.br>
2021-09-07 18:41:55 -0300

    Criando uma Toolbar

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
