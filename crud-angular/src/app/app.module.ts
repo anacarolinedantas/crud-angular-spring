3ee07db70f98fc3b8b8ab93e0651916f786a03fc
Caroline Dantas <caroline.dantas@esig.com.br>
2021-09-20 16:28:42 -0300

    Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
@@ -11,9 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
